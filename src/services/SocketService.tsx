a62adb57267096ffb5284bbe7ae75a60e5cbdfc8
ch02w <scho020224@gmail.com>
2024-07-25 11:19:30 +0900

    rebuild22

--- src/services/SocketService.tsx
+++ src/services/SocketService.tsx
@@ -4,7 +4,7 @@ class SocketService {
   socket: Socket;
 
   constructor() {
-    this.socket = io(process.env.REACT_APP_SOCKET_URL || 'http://52.79.208.231:3001', {
+    this.socket = io('http://52.79.208.231:3001', {
       transports: ['websocket'],
       reconnectionAttempts: 5,
       timeout: 20000,
